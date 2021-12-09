ff9f5334c2208beea062ec79a17cd4e0bf89d1f6
bryan mcwatters <bmcwatters@comcast.net>
2021-12-09 16:20:13 -0500

    initial commit

---
+++ EncapsulationExercise/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExercise
+{
+     public class BankAccount
+    {
+        private double _balance;
+
+        public BankAccount(double balance)
+        {
+            _balance = balance;
+        }
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Simulating a deposit of {amount, 0:c} to your account");
+            _balance += amount;
+
+        }
+
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+
+
+
+
+    }
+}
---
+++ EncapsulationExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EncapsulationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount(0);
+
+            Console.WriteLine("How much money would you like to deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"Your current balance is: {userBalance, 0:c}");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
